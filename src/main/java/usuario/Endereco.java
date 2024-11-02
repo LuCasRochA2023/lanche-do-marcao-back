df89b5f4e33994933d0a6d47f5f7971c3f35f0e9
lucas <mrlukas2012@hotmail.com>
2024-11-02 19:17:47 -0400

    mexendo

--- src/main/java/usuario/Endereco.java
+++ src/main/java/usuario/Endereco.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
+@NoArgsConstructor
 public class Endereco {
     private String complemento;
     private String numero;
